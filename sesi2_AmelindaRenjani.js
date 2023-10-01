4a2dc827997def433d395b6db65f78fd4404089c
Amelindarenjani <114630196+Amelindarenjani@users.noreply.github.com>
2023-10-01 09:33:56 +0700

    Create sesi2_AmelindaRenjani.js

---
+++ sesi2_AmelindaRenjani.js
@@ -0,0 +1,11 @@
+var firstName = "Amelinda"
+var lastName = "Renjani"
+var age = 20
+var isMarried = false
+   
+var statusPernikahan = isMarried? "Menikah" : "Lajang"
+
+console.log(`Nama depan: ${firstName} `)
+console.log(`Nama Belakang : ${lastName}`)
+console.log(`Usia : ${age}`)
+console.log(`Status : ${statusPernikahan}`)
